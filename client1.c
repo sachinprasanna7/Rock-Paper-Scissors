656c1b95b8225e2f52182cc2890f959242f02c9e
Sachin <sachincodes1@gmail.com>
2023-01-14 02:02:57 +0530

    Made the text sent back to the clients more visibile

--- client1.c
+++ client1.c
@@ -4,43 +4,50 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main(int argc, char *argv[]) {
-WSADATA wsa;
-SOCKET socket_client;
-struct sockaddr_in server;
-char move[10];
+int main(int argc, char *argv[])
+{
+    WSADATA wsa;
+    SOCKET socket_client;
+    struct sockaddr_in server;
+    char move[10], result[200];
 
-printf("Initializing Winsock...\n");
-if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
-    printf("Failed. Error Code : %d", WSAGetLastError());
-    return 1;
-}
-printf("Initialized.\n");
+    printf("Initializing Winsock...\n");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("Failed. Error Code : %d", WSAGetLastError());
+        return 1;
+    }
+    printf("Initialized.\n");
 
-// Create a socket
-if((socket_client = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-    printf("Could not create socket : %d", WSAGetLastError());
-}
-printf("Socket created.\n");
+    // Create a socket
+    if ((socket_client = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d", WSAGetLastError());
+    }
+    printf("Socket created.\n");
 
-server.sin_addr.s_addr = inet_addr("127.0.0.1");
-server.sin_family = AF_INET;
-server.sin_port = htons(8888);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888);
 
-// Connect to remote server
-if (connect(socket_client, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    printf("Connect failed. Error Code : %d", WSAGetLastError());
-    return 1;
-}
-printf("Connected.\n");
+    // Connect to remote server
+    if (connect(socket_client, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        printf("Connect failed. Error Code : %d", WSAGetLastError());
+        return 1;
+    }
+    printf("Connected.\n");
 
-while (1) {
-    printf("Make your move (rock, paper, or scissors): ");
-    scanf("%s", move);
-    send(socket_client, move, sizeof(move), 0);
-}
+    while (1)
+    {
+        printf("Make your move (rock, paper, or scissors): ");
+        scanf("%s", move);
+        send(socket_client, move, sizeof(move), 0);
+        recv(socket_client, result, sizeof(result), 0);
+        printf("\n%s\n\n", result);
+    }
 
-closesocket(socket_client);
-WSACleanup();
-return 0;
+    closesocket(socket_client);
+    WSACleanup();
+    return 0;
 }
--- server.c
+++ server.c
@@ -4,20 +4,25 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main(){
+int main()
+{
     WSADATA wsa;
     SOCKET server_socket, client_socket1, client_socket2;
     struct sockaddr_in server, client1, client2;
+    char player1_move[10], player2_move[10], result[200];
+    int player1_points = 0, player2_points = 0;
 
     printf("Initializing Winsock...\n");
-    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
         printf("Failed. Error Code : %d", WSAGetLastError());
         return 1;
     }
     printf("Initialized.\n");
 
     // Create a socket
-    if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
         printf("Could not create socket : %d", WSAGetLastError());
     }
     printf("Socket created.\n");
@@ -28,7 +33,8 @@ int main(){
     server.sin_port = htons(8888);
 
     // Bind the socket
-    if(bind(server_socket, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
+    if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+    {
         printf("Bind failed with error code : %d", WSAGetLastError());
         exit(EXIT_FAILURE);
     }
@@ -45,57 +51,90 @@ int main(){
     client_socket2 = accept(server_socket, (struct sockaddr *)&client2, &c);
     printf("Connection accepted from client 2.\n");
 
-    int player1_points = 0, player2_points = 0;
-    char player1_move[10], player2_move[10];
-
-    while (1) {
+    while (1)
+    {
         recv(client_socket1, player1_move, 10, 0);
         recv(client_socket2, player2_move, 10, 0);
         printf("Player 1 chose: %s\nPlayer 2 chose: %s\n", player1_move, player2_move);
 
-        if (strcmp(player1_move, "rock") == 0) {
-            if (strcmp(player2_move, "scissors") == 0) {
+        if (strcmp(player1_move, "rock") == 0)
+        {
+            if (strcmp(player2_move, "scissors") == 0)
+            {
                 printf("Player 1 wins!\n");
                 player1_points++;
-            } else if (strcmp(player2_move, "paper") == 0) {
+                sprintf(result, "Player 1 wins!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+            else if (strcmp(player2_move, "paper") == 0)
+            {
                 printf("Player 2 wins!\n");
                 player2_points++;
-            } else {
-                printf("Tie!\n");
+                sprintf(result, "Player 2 wins!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
             }
-        } else if (strcmp(player1_move, "scissors") == 0) {
-if (strcmp(player2_move, "paper") == 0) {
-printf("Player 1 wins!\n");
-player1_points++;
-} else if (strcmp(player2_move, "rock") == 0) {
-printf("Player 2 wins!\n");
-player2_points++;
-} else {
-printf("Tie!\n");
-}
-} else if (strcmp(player1_move, "paper") == 0) {
-if (strcmp(player2_move, "rock") == 0) {
-printf("Player 1 wins!\n");
-player1_points++;
-} else if (strcmp(player2_move, "scissors") == 0) {
-printf("Player 2 wins!\n");
-player2_points++;
-} else {
-printf("Tie!\n");
-}
-}
+            else
+            {
+                printf("Tie!\n");
+                sprintf(result, "Tie!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+        }
+        else if (strcmp(player1_move, "scissors") == 0)
+        {
+            if (strcmp(player2_move, "paper") == 0)
+            {
+                printf("Player 1 wins!\n");
+                player1_points++;
+                sprintf(result, "Player 1 wins!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+            else if (strcmp(player2_move, "rock") == 0)
+            {
+                printf("Player 2 wins!\n");
+                player2_points++;
+                sprintf(result, "Player 2 wins!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+            else
+            {
+                printf("Tie!\n");
+                sprintf(result, "Tie!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+        }
+        else if (strcmp(player1_move, "paper") == 0)
+        {
+            if (strcmp(player2_move, "rock") == 0)
+            {
+                printf("Player 1 wins!\n");
+                player1_points++;
+                sprintf(result, "Player 1 wins!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+            else if (strcmp(player2_move, "scissors") == 0)
+            {
+                printf("Player 2 wins!\n");
+                player2_points++;
+                sprintf(result, "Player 2 wins!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+            else
+            {
+                printf("Tie!\n");
+                sprintf(result, "Tie!\nCurrent score: Player 1: %d ; Player 2: %d", player1_points, player2_points);
+            }
+        }
 
-if (player1_points == 5) {
-        printf("Player 1 wins the game!\n");
-        break;
-    } else if (player2_points == 5) {
-        printf("Player 2 wins the game!\n");
-        break;
+        send(client_socket1, result, sizeof(result), 0);
+        send(client_socket2, result, sizeof(result), 0);
+
+        if (player1_points == 5)
+        {
+            printf("Player 1 wins the game!\n\nTHANK YOU!\n");
+            break;
+        }
+        else if (player2_points == 5)
+        {
+            printf("Player 2 wins the game!\n\nTHANK YOU!\n");
+            break;
+        }
     }
-}
 
-closesocket(client_socket1);
-closesocket(client_socket2);
-WSACleanup();
-return 0;
+    closesocket(client_socket1);
+    closesocket(client_socket2);
+    WSACleanup();
+    return 0;
 }
